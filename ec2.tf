7453046b93defec48e8e45e75708e5d8df39d365
Sravani <sravanivakadani8114@gmail.com>
2023-06-20 18:30:37 +0530

    loadbalancer creation

---
+++ ec2.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
+}
+
+resource "aws_instance" "Krishna" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instances"
+  }
+}
---
+++ loadbalancer.tf
@@ -0,0 +1,42 @@
+resource "aws_lb_target_group" "instance" {
+  name        = "TGRP_instance"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = "${aws_vpc.myvpc.id}"
+}
+
+resource "aws_lb" "NLB" {
+  name               = "Network_LB"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = ["${aws_subnet.subnet01.id}"]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "NLBA" {
+  target_group_arn = "${aws_lb_target_group.instance.arn}"
+  target_id        = "${aws_instance.Krishna.id}"
+  port             = 80
+}
+
+resource "aws_lb_listener" "NLBL" {
+  load_balancer_arn = "${aws_lb_listener.NLBL.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
