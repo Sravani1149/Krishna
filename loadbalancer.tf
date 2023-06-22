6b681949978ba12322144370cba7decb714310c5
Sravani1149 <135800376+Sravani1149@users.noreply.github.com>
2023-06-22 20:38:59 +0530

    changes added

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -21,13 +21,13 @@ resource "aws_lb" "NLB" {
 }
 
 resource "aws_lb_target_group_attachment" "NLBA" {
-  target_group_arn = "${aws_lb_target_group.TGRP_instance.arn}"
-  target_id        = "${aws_instance.Krishna.id}"
+  target_group_arn = aws_lb_target_group.TGRP_instance.arn
+  target_id        = aws_instance.Krishna.id
   port             = 80
 }
 
 resource "aws_lb_listener" "NLBL" {
-  load_balancer_arn = "${aws_lb.NLB.arn}"
+  load_balancer_arn = aws_lb.NLB.arn
 port = 80  
 protocol          = "TCP"
 default_action {
